095df04972c80dceb63d24ecaf4ee1bfd80299a7
André Keßler <andre.kessler91@gmx.de>
2026-04-09 18:15:04 +0200

    Implemented simple script to trigger Grove Humdifier

---
+++ SimpleHumidifier/SimpleHumidifier.ino
@@ -0,0 +1,20 @@
+#define LED_BUILTIN 21  // Onboard LED is connected to GPIO21 on XIAO ESP32S3
+#define HUMIDIFIER A0
+
+void setup() {
+  Serial.begin(115200);        // Start serial communication at 115200 baud rate
+  pinMode(LED_BUILTIN, OUTPUT); // Set the LED pin as an output
+  pinMode(HUMIDIFIER, OUTPUT);
+  Serial.println("Hello from XIAO ESP32S3!"); // Print message once on start
+}
+
+void loop() {
+  Serial.println("Power on Humidifier for 30 seconds");
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(HUMIDIFIER, HIGH);
+  delay(30000);
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(HUMIDIFIER, LOW);
+  Serial.println("Sleep for 5 seconds");
+  delay(5000);
+}
