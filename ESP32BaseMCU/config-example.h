78dd73a1960dc90fdb6721c46c9db4e755a243c0
André Kessler <andre.kessler91@gmx.de>
2026-05-03 17:54:13 +0200

    Improved the ESP32BaseMCU code, to publish and receive messages from AWS IOT Core

--- ESP32BaseMCU/config-example.h
+++ ESP32BaseMCU/config-example.h
@@ -3,7 +3,10 @@
 #define WIFI_SSID "YOUR_WIFI_NAME"
 #define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
 #define AWS_IOT_ENDPOINT "xxxxxxxxxxxxx-ats.iot.eu-north-1.amazonaws.com"
-#define THINGNAME "incubator-001"
+#define THINGNAME "incubator-test-001"
+#define AWS_IOT_PUBLISH_TOPIC   "incubator/data"
+#define AWS_IOT_SUBSCRIBE_TOPIC "incubator/" THINGNAME "/cmd"
+#define AWS_IOT_STATUS_TOPIC    "incubator/" THINGNAME "/status"
 
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
