357d88ee4c4ec7fbc3359350791ace63537d133d
André Kessler <andre.kessler91@gmx.de>
2026-05-18 21:35:04 +0200

    Added prefix to various variables of the water-level sensor

--- WaterLevelSensorSimplePercentage/WaterLevelSensorSimplePercentage.ino
+++ WaterLevelSensorSimplePercentage/WaterLevelSensorSimplePercentage.ino
@@ -1,53 +1,48 @@
 #include <Wire.h>
 
-#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-#define SERIAL_PORT SerialUSB
-#else
-#define SERIAL_PORT Serial
-#endif
+#define WTR_LVL_THRESHOLD 100
+#define WTR_LVL_HIGH_ADDR 0x78
+#define WTR_LVL_LOW_ADDR  0x77
 
-#define THRESHOLD 100
-#define HIGH_ADDR 0x78
-#define LOW_ADDR  0x77
+byte WTR_LVL_lowData[8];
+byte WTR_LVL_highData[12];
 
-byte lowData[8], highData[12];
-
-void readBytes(byte addr, byte *buf, byte len) {
+void WTR_LVL_readBytes(byte addr, byte *buf, byte len) {
   Wire.requestFrom(addr, len);
   while (Wire.available() < len);
   for (byte i = 0; i < len; i++) buf[i] = Wire.read();
 }
 
-int readWaterLevelPercent() {
-  uint32_t mask = 0;
+int WTR_LVL_readPercent() {
+  uint32_t WTR_LVL_mask = 0;
 
-  readBytes(LOW_ADDR, lowData, 8);
-  readBytes(HIGH_ADDR, highData, 12);
+  WTR_LVL_readBytes(WTR_LVL_LOW_ADDR, WTR_LVL_lowData, 8);
+  WTR_LVL_readBytes(WTR_LVL_HIGH_ADDR, WTR_LVL_highData, 12);
 
   for (byte i = 0; i < 8; i++) {
-    if (lowData[i] > THRESHOLD) mask |= (1UL << i);
+    if (WTR_LVL_lowData[i] > WTR_LVL_THRESHOLD) WTR_LVL_mask |= (1UL << i);
   }
 
   for (byte i = 0; i < 12; i++) {
-    if (highData[i] > THRESHOLD) mask |= (1UL << (8 + i));
+    if (WTR_LVL_highData[i] > WTR_LVL_THRESHOLD) WTR_LVL_mask |= (1UL << (8 + i));
   }
 
-  byte sections = 0;
-  while (mask & 1) {
-    sections++;
-    mask >>= 1;
+  byte WTR_LVL_sections = 0;
+  while (WTR_LVL_mask & 1) {
+    WTR_LVL_sections++;
+    WTR_LVL_mask >>= 1;
   }
 
-  return sections * 5;
+  return WTR_LVL_sections * 5;
 }
 
 void setup() {
-  SERIAL_PORT.begin(115200);
+  Serial.begin(115200);
   Wire.begin();
 }
 
 void loop() {
-  SERIAL_PORT.print(readWaterLevelPercent());
-  SERIAL_PORT.println("%");
+  Serial.print(WTR_LVL_readPercent());
+  Serial.println("%");
   delay(10000);
 }
